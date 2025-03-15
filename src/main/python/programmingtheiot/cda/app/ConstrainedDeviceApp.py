84576c1f0f9d7642edde086ea46d15cba1f196ce
MiniX16 <javivi004.ou@gmail.com>
2025-03-15 15:57:46 +0100

    PIOT-DCA-02 until 004

--- src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
+++ src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
@@ -1,87 +1,55 @@
-#####
-# 
-# This class is part of the Programming the Internet of Things
-# project, and is available via the MIT License, which can be
-# found in the LICENSE file at the top level of this repository.
-# 
-# You may find it more helpful to your design to adjust the
-# functionality, constants and interfaces (if there are any)
-# provided within in order to meet the needs of your specific
-# Programming the Internet of Things project.
-# 
-
 import logging
-
 from time import sleep
+from programmingtheiot.cda.system.SystemPerformanceManager import SystemPerformanceManager
 
-logging.basicConfig(format = '%(asctime)s:%(name)s:%(levelname)s:%(message)s', level = logging.DEBUG)
+logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s:%(message)s', level=logging.DEBUG)
 
-class ConstrainedDeviceApp():
-	"""
-	Definition of the ConstrainedDeviceApp class.
-	
-	"""
-	
-	def __init__(self):
-		"""
-		Initialization of class.
-		
-		@param path The name of the resource to apply to the URI.
-		"""
-		logging.info("Initializing CDA...")
-		
-		# TODO: implementation here
+class ConstrainedDeviceApp:
+    """
+    Definition of the ConstrainedDeviceApp class.
+    """
 
-	def startApp(self):
-		"""
-		Start the CDA. Calls startManager() on the device data manager instance.
-		
-		"""
-		logging.info("Starting CDA...")
-		
-		# TODO: implementation here
-		
-		logging.info("CDA started.")
+    def __init__(self):
+        logging.info("Initializing CDA...")
+        self.sysPerfMgr = SystemPerformanceManager()
 
-	def stopApp(self, code: int):
-		"""
-		Stop the CDA. Calls stopManager() on the device data manager instance.
-		
-		"""
-		logging.info("CDA stopping...")
-		
-		# TODO: implementation here
-		
-		logging.info("CDA stopped with exit code %s.", str(code))
-		
-	def parseArgs(self, args):
-		"""
-		Parse command line args.
-		
-		@param args The arguments to parse.
-		"""
-		logging.info("Parsing command line args...")
+    def startApp(self):
+        logging.info("Starting CDA...")
+        self.sysPerfMgr.startManager()
+        logging.info("CDA started.")
+
+    def stopApp(self, code: int):
+        logging.info("CDA stopping...")
+        self.sysPerfMgr.stopManager()
+        logging.info("CDA stopped with exit code %s.", str(code))
+
+    def parseArgs(self, args):
+        """
+        Parse command line args.
+        
+        @param args: The arguments to parse.
+        """
+        logging.info("Parsing command line args...")
 
 
 def main():
-	"""
-	Main function definition for running client as application.
-	
-	Current implementation runs for 35 seconds then exits.
-	"""
-	cda = ConstrainedDeviceApp()
-	cda.startApp()
-	
-	# run for 10 seconds - this can be changed as needed
-	sleep(10)
-	
-	# optionally stop the app - this can be removed if needed
-	cda.stopApp(0)
+    """
+    Main function definition for running client as application.
+
+    Current implementation runs for 65 seconds then exits.
+    """
+    cda = ConstrainedDeviceApp()
+    cda.startApp()
+
+    # Run for 65 seconds - this can be changed as needed
+    sleep(65)
+
+    # Optionally stop the app - this can be removed if needed
+    cda.stopApp(0)
+
 
 if __name__ == '__main__':
-	"""
-	Attribute definition for when invoking as app via command line
-	
-	"""
-	main()
-	
+    """
+    Attribute definition for when invoking as app via command line.
+    """
+    main()
--- src/main/python/programmingtheiot/cda/system/SystemPerformanceManager.py
+++ src/main/python/programmingtheiot/cda/system/SystemPerformanceManager.py
@@ -1,12 +1,3 @@
-#####
-# 
-# This class is part of the Programming the Internet of Things project.
-# 
-# It is provided as a simple shell to guide the student and assist with
-# implementation for the Programming the Internet of Things exercises,
-# and designed to be modified by the student as needed.
-#
-
 import logging
 
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -22,22 +13,38 @@ from programmingtheiot.cda.system.SystemMemUtilTask import SystemMemUtilTask
 from programmingtheiot.data.SystemPerformanceData import SystemPerformanceData
 
 class SystemPerformanceManager(object):
-	"""
-	Shell representation of class for student implementation.
-	
-	"""
+    """
+    Shell representation of class for student implementation.
+    """
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        configUtil = ConfigUtil()
 
-	def handleTelemetry(self):
-		pass
-		
-	def setDataMessageListener(self, listener: IDataMessageListener) -> bool:
-		pass
-	
-	def startManager(self):
-		pass
-		
-	def stopManager(self):
-		pass
+        self.pollRate = configUtil.getInteger(
+            section=ConfigConst.CONSTRAINED_DEVICE, 
+            key=ConfigConst.POLL_CYCLES_KEY, 
+            defaultVal=ConfigConst.DEFAULT_POLL_CYCLES
+        )
+
+        self.locationID = configUtil.getProperty(
+            section=ConfigConst.CONSTRAINED_DEVICE, 
+            key=ConfigConst.DEVICE_LOCATION_ID_KEY, 
+            defaultVal=ConfigConst.NOT_SET
+        )
+
+        if self.pollRate <= 0:
+            self.pollRate = ConfigConst.DEFAULT_POLL_CYCLES
+
+        self.dataMsgListener = None
+
+    def handleTelemetry(self):
+        pass
+
+    def setDataMessageListener(self, listener: IDataMessageListener) -> bool:
+        pass
+
+    def startManager(self):
+        logging.info("Started SystemPerformanceManager.")
+
+    def stopManager(self):
+        logging.info("Stopped SystemPerformanceManager.")
