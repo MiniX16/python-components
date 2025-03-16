167887d6e32de91d8f438783796eb4e214d2417e
MiniX16 <javivi004.ou@gmail.com>
2025-03-17 00:59:09 +0100

    PIOT-CDA-03-009

--- src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
+++ src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
@@ -1,55 +1,51 @@
+import sys
 import logging
+
 from time import sleep
-from programmingtheiot.cda.system.SystemPerformanceManager import SystemPerformanceManager
 
-logging.basicConfig(format='%(asctime)s:%(name)s:%(levelname)s:%(message)s', level=logging.DEBUG)
+import programmingtheiot.common.ConfigConst as ConfigConst
 
-class ConstrainedDeviceApp:
-    """
-    Definition of the ConstrainedDeviceApp class.
-    """
+from programmingtheiot.common.ConfigUtil import ConfigUtil
+from programmingtheiot.cda.app.DeviceDataManager import DeviceDataManager
 
+logging.basicConfig(format = '%(asctime)s:%(name)s:%(levelname)s:%(message)s', level = logging.DEBUG)
+
+class ConstrainedDeviceApp():
     def __init__(self):
         logging.info("Initializing CDA...")
-        self.sysPerfMgr = SystemPerformanceManager()
+
+        self.dataMgr = DeviceDataManager()
 
     def startApp(self):
         logging.info("Starting CDA...")
-        self.sysPerfMgr.startManager()
+
+        self.dataMgr.startManager()
+
         logging.info("CDA started.")
 
     def stopApp(self, code: int):
         logging.info("CDA stopping...")
-        self.sysPerfMgr.stopManager()
+
+        self.dataMgr.stopManager()
+
         logging.info("CDA stopped with exit code %s.", str(code))
 
-    def parseArgs(self, args):
-        """
-        Parse command line args.
-        
-        @param args: The arguments to parse.
-        """
-        logging.info("Parsing command line args...")
-
-
 def main():
-    """
-    Main function definition for running client as application.
-
-    Current implementation runs for 65 seconds then exits.
-    """
     cda = ConstrainedDeviceApp()
     cda.startApp()
 
-    # Run for 65 seconds - this can be changed as needed
-    sleep(65)
+    runForever = ConfigUtil().getBoolean(ConfigConst.CONSTRAINED_DEVICE, ConfigConst.RUN_FOREVER_KEY)
 
-    # Optionally stop the app - this can be removed if needed
-    cda.stopApp(0)
+    if runForever:
+        while True:
+            sleep(5)
+    else:
+        if len(sys.argv) != 2:
+            print("NEED TO SET A TIMER AS AN ARGUMENT.")
+            return
 
+        sleep(int(sys.argv[1]))
+        cda.stopApp(0)
 
 if __name__ == '__main__':
-    """
-    Attribute definition for when invoking as app via command line.
-    """
     main()
