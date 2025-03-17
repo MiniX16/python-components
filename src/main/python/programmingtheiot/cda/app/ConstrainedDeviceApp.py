be74a8a38ea13809c4e1b8fc1f07d95b39fea793
MiniX16 <javivi004.ou@gmail.com>
2025-03-17 01:45:47 +0100

    PIOT-CDA-03-009

--- src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
+++ src/main/python/programmingtheiot/cda/app/ConstrainedDeviceApp.py
@@ -1,4 +1,3 @@
-import sys
 import logging
 
 from time import sleep
@@ -11,41 +10,39 @@ from programmingtheiot.cda.app.DeviceDataManager import DeviceDataManager
 logging.basicConfig(format = '%(asctime)s:%(name)s:%(levelname)s:%(message)s', level = logging.DEBUG)
 
 class ConstrainedDeviceApp():
-    def __init__(self):
-        logging.info("Initializing CDA...")
+	def __init__(self):
+		logging.info("Initializing CDA...")
 
-        self.dataMgr = DeviceDataManager()
+		self.dataMgr = DeviceDataManager()
 
-    def startApp(self):
-        logging.info("Starting CDA...")
+	def startApp(self):
+		logging.info("Starting CDA...")
 
-        self.dataMgr.startManager()
+		self.dataMgr.startManager()
 
-        logging.info("CDA started.")
+		logging.info("CDA started.")
 
-    def stopApp(self, code: int):
-        logging.info("CDA stopping...")
+	def stopApp(self, code: int):
+		logging.info("CDA stopping...")
 
-        self.dataMgr.stopManager()
+		self.dataMgr.stopManager()
 
-        logging.info("CDA stopped with exit code %s.", str(code))
+		logging.info("CDA stopped with exit code %s.", str(code))
 
 def main():
-    cda = ConstrainedDeviceApp()
-    cda.startApp()
+	cda = ConstrainedDeviceApp()
+	cda.startApp()
 
-    runForever = ConfigUtil().getBoolean(ConfigConst.CONSTRAINED_DEVICE, ConfigConst.RUN_FOREVER_KEY)
+	runForever = ConfigUtil().getBoolean(ConfigConst.CONSTRAINED_DEVICE, ConfigConst.RUN_FOREVER_KEY)
 
-    if runForever:
-        while True:
-            sleep(5)
-    else:
-        if len(sys.argv) != 2:
-            print("NEED TO SET A TIMER AS AN ARGUMENT.")
-            return
+	if runForever:
+		while (True):
+			sleep(5)
 
-        sleep(int(sys.argv[1]))
-        cda.stopApp(0)
+	else:
+		# TODO: Make the '65' value configurable
+		sleep(65)
+		cda.stopApp(0)
 
 if __name__ == '__main__':
-    main()
+	main()
