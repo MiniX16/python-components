146a80c81523a270a8c3602247fad1783c3af105
MiniX16 <javivi004.ou@gmail.com>
2025-03-21 19:02:45 +0100

    lab05 completo

--- src/main/python/programmingtheiot/data/DataUtil.py
+++ src/main/python/programmingtheiot/data/DataUtil.py
@@ -8,38 +8,88 @@
 #
 
 from json import JSONEncoder
+import json
+import logging
+
+from decimal import Decimal
 
 from programmingtheiot.data.ActuatorData import ActuatorData
 from programmingtheiot.data.SensorData import SensorData
 from programmingtheiot.data.SystemPerformanceData import SystemPerformanceData
 
 class DataUtil():
-	"""
-	Shell representation of class for student implementation.
-	
-	"""
+    """
+    Shell representation of class for student implementation.
+    """
 
-	def __init__(self, encodeToUtf8 = False):
-		pass
-	
-	def actuatorDataToJson(self, data: ActuatorData = None):
-		pass
-	
-	def sensorDataToJson(self, data: SensorData = None):
-		pass
+    def __init__(self, encodeToUtf8 = False):
+        pass
 
-	def systemPerformanceDataToJson(self, data: SystemPerformanceData = None):
-		pass
+    def actuatorDataToJson(self, data: ActuatorData = None, useDecForFloat: bool = False):
+        if data is None:
+            return None
+        if useDecForFloat:
+            return json.dumps(data, indent=4, cls=JsonDataEncoder, use_decimal=True)
+        else:
+            return json.dumps(data, indent=4, cls=JsonDataEncoder)
+
+    def jsonToActuatorData(self, jsonData: str = None, useDecForFloat: bool = False):
+        if jsonData is None or jsonData.strip() == "":
+            return None
+        jsonData = jsonData.replace("\'", "\"").replace('False', 'false').replace('True', 'true')
+        jsonStruct = json.loads(jsonData)
+        ad = ActuatorData()
+        varStruct = vars(ad)
+
+        for key in jsonStruct:
+            if key in varStruct:
+                setattr(ad, key, jsonStruct[key])
+        return ad
+
+    def sensorDataToJson(self, data: SensorData = None, useDecForFloat: bool = False):
+        if data is None:
+            return None
+        if useDecForFloat:
+            return json.dumps(data, indent=4, cls=JsonDataEncoder, use_decimal=True)
+        else:
+            return json.dumps(data, indent=4, cls=JsonDataEncoder)
+
+    def systemPerformanceDataToJson(self, data: SystemPerformanceData = None, useDecForFloat: bool = False):
+        if data is None:
+            return None
+        if useDecForFloat:
+            return json.dumps(data, indent=4, cls=JsonDataEncoder, use_decimal=True)
+        else:
+            return json.dumps(data, indent=4, cls=JsonDataEncoder)
+
+    def jsonToSensorData(self, jsonData: str = None, useDecForFloat: bool = False):
+        if jsonData is None or jsonData.strip() == "":
+            return None
+        jsonData = jsonData.replace("\'", "\"").replace('False', 'false').replace('True', 'true')
+        jsonStruct = json.loads(jsonData)
+        sd = SensorData()
+        varStruct = vars(sd)
+
+        for key in jsonStruct:
+            if key in varStruct:
+                setattr(sd, key, jsonStruct[key])
+        return sd
+
+    def jsonToSystemPerformanceData(self, jsonData: str = None, useDecForFloat: bool = False):
+        if jsonData is None or jsonData.strip() == "":
+            return None
+        jsonData = jsonData.replace("\'", "\"").replace('False', 'false').replace('True', 'true')
+        jsonStruct = json.loads(jsonData)
+        spd = SystemPerformanceData()
+        varStruct = vars(spd)
+
+        for key in jsonStruct:
+            if key in varStruct:
+                setattr(spd, key, jsonStruct[key])
+        return spd
+
 	
-	def jsonToActuatorData(self, jsonData: str = None):
-		pass
-	
-	def jsonToSensorData(self, jsonData: str = None):
-		pass
-	
-	def jsonToSystemPerformanceData(self, jsonData: str = None):
-		pass
-	
+
 class JsonDataEncoder(JSONEncoder):
 	"""
 	Convenience class to facilitate JSON encoding of an object that
